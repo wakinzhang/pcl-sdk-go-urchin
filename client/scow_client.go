4718d493ea833bc865941a510b98a3636c53895b
wakinzhang <287870120@qq.com>
2025-05-09 18:07:23 +0800

    ci for save

--- client/scow_client.go
+++ client/scow_client.go
@@ -963,28 +963,11 @@ func (o *ScowClient) DownloadChunks(
 	}
 
 	if http.StatusOK != response.StatusCode {
-		defer func(body io.ReadCloser) {
-			_err := body.Close()
-			if nil != _err {
-				Logger.WithContext(ctx).Error(
-					"io.ReadCloser failed.",
-					" err: ", _err)
-			}
-		}(response.Body)
-
-		respBody, err := io.ReadAll(response.Body)
-		if nil != err {
-			Logger.WithContext(ctx).Error(
-				"io.ReadAll failed.",
-				" err: ", err)
-			return err, output
-		}
 		Logger.WithContext(ctx).Error(
 			"ScowClient.DownloadChunks response failed.",
 			" StatusCode: ", response.StatusCode,
-			" Status: ", response.Status,
-			" respBody: ", string(respBody))
-		return errors.New(string(respBody)), output
+			" Status: ", response.Status)
+		return errors.New("DownloadChunks failed"), output
 	}
 
 	output = new(ScowDownloadPartOutput)
